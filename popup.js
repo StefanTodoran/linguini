8f47a6b9919540e453337da064afba647c3349ba
spicyshrek <spicyshrek420@gmail.com>
2020-10-17 14:22:44 -0700

    some comments and a jank fix thing

--- popup.js
+++ popup.js
@@ -3,15 +3,33 @@
 let changeColor = document.getElementById('changeColor');
 
 chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+	changeColor.style.backgroundColor = data.color;
+	changeColor.setAttribute('value', data.color);
 });
 
 changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
+	let color = element.target.value;
+
+	chrome.tabs.executeScript( {
+		code: "window.getSelection().toString();"
+	}, function(selection) {
+		document.getElementById('output').value = selection[0];
+	});
+
+	//this here sets background color to something different, we don't need and will remove
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{code: 'document.body.style.backgroundColor = "' + color + '";'});
+	});
 };
+
+
+//alternative selection method, not sure how to implement
+/*
+let el = activeWindow.document.activeElement; 	
+if (isTextElem(el)) { 		
+	if ('selectionStart' in el && el.selectionStart !== el.selectionEnd) { 			
+		return el.value.substring(el.selectionStart, el.selectionEnd); 		
+	} 	
+}*/
